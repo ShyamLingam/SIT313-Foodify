2ddc8aeb9a671ab4fa3141da963224b0334ace9b
Vishnu Prabhakaran <vprabha@deakin.edu.au>
2018-09-06 22:50:27 +1000

    Basic 5 Tabs working

---
+++ foodify/foodify/CompletedPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace foodify
+{
+    public partial class CompletedPage : ContentPage
+    {
+        public CompletedPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- foodify/foodify/MainPage.xaml.cs
+++ foodify/foodify/MainPage.xaml.cs
@@ -4,14 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 
 namespace foodify
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : Xamarin.Forms.TabbedPage
     {
         public MainPage()
         {
             InitializeComponent();
+
+            On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom)
+                .SetBarItemColor(Color.Black)
+                .SetBarSelectedItemColor(Color.Green);
         }
     }
 }
