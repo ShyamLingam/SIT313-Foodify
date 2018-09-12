c458a7a90aa09bd543ed04c58b81200fe522f967
Vishnu Prabhakaran <vprabha@deakin.edu.au>
2018-09-13 00:27:42 +1000

    Added Logos, Api fetch, View Models Resetpage, Login page, register page, Home page

---
+++ foodify/foodify/ApiList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace foodify
+{
+    public class ApiList : ContentPage
+    {
+        public ApiList()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
---
+++ foodify/foodify/Model/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace FitnessGuru.Model
+{
+    public class Constants
+    {
+        public static bool isDev = true;
+
+        public static Color BackgroundColor = Color.FromRgb(51, 51, 51);
+        public static Color MainTextColor = Color.White;
+        public static Color GreenTextColor = Color.FromRgb(0, 167, 0);
+        public static int LoginIconHeight = 120;
+    }
+}
